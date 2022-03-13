3f7f1650d060c09f7c48fd3265d60c33a23d386c
The Arcterion Project <shaynemganness@gmail.com>
2022-03-12 22:55:22 -0500

    questions js file created

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+var questions = [
+    {
+      title: "JavaScript is what type of language?",
+      options: ["Object-Oriented", "Object-Based", "Assembly-language", "High-level"],
+      answer: "Object-Based"
+    },
+    {
+      title: "Which one of the following is also known as Conditional Expression?",
+      options: ["Alternative to if-else", "Switch statement", "If-then-else statement", "immediate if"],
+      answer: "immediate if"
+    },
+    {
+      title: "In JavaScript, what is a block of statement?",
+      options: ["Conditional block", "a block that combines a number of statements into a single compound statement", "both a conditional block and a single statement", "a block that contains a single statement"],
+      answer: "a block that combines a number of statements into a single compound statement"
+    },
+    {
+      title: "When interpreter encounters an empty statements, what it will do?",
+      options: ["Show a warning", "Prompt to complete the statement", "Throw an error", "Ignore the statements"],
+      answer: "Ignore the statements"
+    },
+    {
+      title: "'Function' and 'var' are known as:",
+      options: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
+      answer: "Declaration statements"
+    },
+    {
+      title: "Which of the following variables takes precedence over the others if the names are the same?",
+      options: ["Global variable", "Local variable", "Both of the above", "None of the above"],
+      answer: "Local variable"
+    },
+    {
+      title: "Which one of the following operators is used to check whether a specific property exists or not?",
+      options: ["exists", "exist", "within", "in"],
+      answer: "in"
+    },
+    {
+      title: "In a case where the value of the operator is NULL, the typeof returned by the unary operator is what?",
+      options: ["undefined", "string", "boolean", "object"],
+      answer: "object"
+    },
+    {
+      title: "Which one of the following is not a keyword/phrase",
+      options: ["if", "with", "debugger", "use strict"],
+      answer: "use strict"
+    },
+    {
+      title: "Which one of the following symbols is used for creating comments in the javascript?",
+      options: ["\\", "//", "``", "*"],
+      answer: "//"
+    },
+    {
+      title: "What are the three important manipulations for a loop on a loop variable?",
+      options: ["Updation, Incrementation, Initialization", "Initialization, Testing, Incrementation", "Testing, Updation, Testing", "Initialization, Testing, Updation"],
+      answer: "Initialization, Testing, Updation"
+    }
+  ];
